97885edf5ac92c2c4e2ddcc6a30dcaf2dde196cb
AlexFeather <alfeat@gmail.com>
2021-08-13 13:45:10 +0300

    Created Metric classes

---
+++ MetricsAgent/Models/Metrics/RamMetrics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Models.Metrics
+{
+    public class RamMetrics
+    {
+        int Id { get; set; }
+        int Value { get; set; }
+        TimeSpan Time { get; set; }
+    }
+}
